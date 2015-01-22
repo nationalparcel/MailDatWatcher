b8f2650e286857e56ee41d208d50b0d0b87aaf40
nationalparcel <it@nationalparcel.com>
2015-01-22 12:04:44 -0500

    Bug fix
    the write SPServices Async true was not writing everything.  Added
    async.js to manage the calls

--- MailDatWatcher/MailDatWatcher/MailDatWatcher.py
+++ MailDatWatcher/MailDatWatcher/MailDatWatcher.py
@@ -4,84 +4,143 @@ import zipfile
 import subprocess
 import shutil
 import time
+import smtplib
 
-#strings
-pathToFtp = "F:\\mailDats"
-pathToDatFolder = "C:\\DATData"
+def sendEmail(message, subject, to):
+    try:
+        sender = 'networkalerts@nationalparcel.com'
+        msg = ("From: DatFileError@nationalparcel.com \n"
+                   "To: smccoy@nationalparcel.com; dgriffin@nationalparcel.com \n"
+                   "Subject: " + subject + "\n"
+                   "\n"
+                   + message)
+        s = smtplib.SMTP('smtp.nationalparcel.com')
+        s.login("networkalerts@nationalparcel.com", "2Simplex")
+        s.sendmail(sender, to, msg)
+    except Exception, err:
+        print str(err)
+    finally:
+        s.quit()
+
+def checkDupUpload(path, fileName):
+    jobName = fileName.lower().replace(".zip", "")
+    pathOnly = path.replace(fileName, "")
+    isThereADup = False
+    for p, s, files in os.walk(pathOnly + "//archive"):
+        for file in files:
+            file = file.lower().replace(".csm", "")
+            file = file.lower().replace(".hdr", "")
+            if jobName == file:
+                isThereADup = True
+                break
+
+    return isThereADup
 
 def unZipFile (filePath):
     try:
-        #zfile = zipfile.ZipFile(filePath)
         fileToUnzip = open(filePath, 'rb')
         zfile = zipfile.ZipFile(fileToUnzip)
         for name in zfile.namelist():
-            if ( name.find(".csm") != -1 or name.find(".hdr") != -1):
+            if ( name.lower().find(".csm") != -1 or name.lower().find(".hdr") != -1):
                 zfile.extract(name, pathToDatFolder)
 
         fileToUnzip.close()
         return
     except Exception, err:
         print str(err)
-        print "zip file done"
+        writeToLog("Zip file error " + str(err) + "-" + filePath)
+        sendEmail("Zip file error - " + str(err), "Zip File Error" + "-" + filePath, ["smccoy@nationalparcel.com", "dgriffin@nationalparcel.com"])
 
 def runCSMToFMS ():
-    for path, subdir, files in os.walk (pathToDatFolder):
+    try:
+        for path, subdir, files in os.walk (pathToDatFolder):
+            for filename in files:
+                if (filename.find(".hdr") != -1):
+                    file = open(path + "\\" + filename)
+                    hdrContents = file.read()
+                    if hdrContents.find("14-1") != -1:
+                        output = subprocess.Popen(["C:\\CustomApps\\MailDirectCSMToFMS\\bin\\ForteenOne\\CSMToFMS.exe"], stdout = subprocess.PIPE)
+                        resualts = output.communicate()[0]
+                        print resualts
+                        output.terminate()
+                    elif hdrContents.find("14-2") != -1:
+                        output = subprocess.Popen(["C:\\CustomApps\\MailDirectCSMToFMS\\bin\\FourteenTwo\\CSMToFMS.exe"], stdout = subprocess.PIPE)
+                        resualts = output.communicate()[0]
+                        print resualts
+                        output.terminate()
+                    else:
+                        resualts = "error"
+
+                    if resualts.find("Finished") == -1:
+                        resualts = "error"
+
+                    print resualts
+                    file.close()
+                    return resualts
+
+    except Exception, err:
+        print str(err)
+        writeToLog(str(err))
+
+def writeToLog(message):
+    logFileLoc = "C:\\Users\\administrator.NPLATL\\Desktop\\Server Side Scripts\\DatWatcher\\Logs\\errorLog.txt"
+    #logFileLoc = "C:\\Users\\Shawn\\Desktop\\Server Side Scripts\\DatWatcher\\Logs\\errorLog.txt"
+    currentDT = str(time.asctime(time.localtime(time.time())))
+    with open(logFileLoc, "a") as logFile:
+        logFile.write("*****\n" + currentDT + " - " + message + "\n\n")
+
+def moveCMSFiles (archOrError, pathDatFolder, clientPath):
+    moveToFolder = "\\archive\\"
+    for p, s, cleanUpFiles in os.walk(pathDatFolder):
+        for datFileName in cleanUpFiles:
+            if archOrError == "e":
+                moveToFolder = "\\Errors\\"
+                writeToLog("Problem with CMStoFMS Program" + datFileName)
+                sendEmail("Problem with CMStoFMS Program " + datFileName, "CMStoFMS Error", errorEmails)
+
+            src = pathDatFolder + "\\" + datFileName
+            dest = clientPath + moveToFolder + datFileName
+            shutil.move(src, dest)
+
+def checkForFileLocks (path):
+    if os.path.exists(path):
+        try:
+            os.rename(path, path + "_")
+            os.rename(path + "_", path)
+            return False
+        except OSError as err:
+            writeToLog("Access to " + path + " is locked. Waiting till next run")
+            return True
+
+#####################################################################################################################################
+
+errorEmails = ["smccoy@nationalparcel.com", "dgriffin@nationalparcel.com"]
+pathToFtp = "F:\\mailDats"
+pathToDatFolder = "C:\\DATData"
+
+try:
+    for path, subdir, files in os.walk(pathToFtp):
         for filename in files:
-            if (filename.find(".hdr") != -1):
-                file = open(path + "\\" + filename)
-                hdrContents = file.read()
-                if hdrContents.find("14-1") != -1:
-                    exedir = "C:\\CustomApps\\MailDirectCSMToFMS\\bin\\FourteenOne\\CSMToFMS.exe"
-                    output = subprocess.Popen([exedir], stdout = subprocess.PIPE)
-                    resualts = output.communicate()[0]
-                    print resualts
-                    output.terminate()
-                elif hdrContents.find("14-2") != -1:
-                    exedir = "C:\\CustomApps\\MailDirectCSMToFMS\\bin\\FourteenTwo\\CSMToFMS.exe"
-                    output = subprocess.Popen([exedir], stdout = subprocess.PIPE)
-                    resualts = output.communicate()[0]
-                    print resualts
-                    output.terminate()
-                else:
-                    resualts = "error"
-
-                if resualts.find("Finished") == -1:
-                    resualts = "error"
-
-                print resualts
-                file.close()
-                return resualts
-
-for path, subdir, files in os.walk(pathToFtp):
-    for filename in files:
-        if filename.find(".zip") != -1 and path.find("archive") == -1:
-            pathToZipFile = path + "\\" + filename
-            unZipFile( pathToZipFile )
-            resaults = runCSMToFMS()
-            try:
-                if resaults != "error":
-                    for p, s, cleanUpFiles in os.walk( pathToDatFolder ):
-                        for datFileName in cleanUpFiles:
-                            src = pathToDatFolder + "\\" + datFileName
-                            print src
-                            dest = path + "\\archive\\" + datFileName
-                            print dest
-                            shutil.move(src, dest)
-                else:
-                    for p, s, cleanUpFiles in os.walk( pathToDatFolder ):
-                        for datFileName in cleanUpFiles:
-                            src = pathToDatFolder + "\\" + datFileName
-                            print src
-                            dest = pathToFtp + "\\Errors\\" + datFileName
-                            print dest
-                            shutil.move(src, dest)
-                            with open("C:\\Users\\administrator.NPLATL\\Desktop\\Server Side Scripts\\DatWatcher\\Logs\\errorLog.txt", "a") as logFile:
-                                logFile.write("*****\n" + str(time.strftime("%d/%m/%Y")) + "\n" + "Problem with CMStoFMS Program - " +datFileName+ "\n \n")
-
+            if filename.find(".zip") != -1 and path.find("archive") == -1 and path.lower().find("errors") == -1:
+                pathToZipFile = path + "\\" + filename
+                isFileLocked = checkForFileLocks(pathToZipFile)
+                if isFileLocked == False:
+                    isThereADup = checkDupUpload(pathToZipFile, filename)
+                    if isThereADup == True:
+                        sendEmail(filename + " is already uploaded", "Dup Upload Detected", ["smccoy@nationalparcel.com", "dgriffin@nationalparcel.com"])
+                        writeToLog(filename + " is already uploaded")
+                        shutil.move(pathToZipFile, pathToFtp + "\\Errors\\" + filename)
+                    else:
+                        unZipFile( pathToZipFile )
+                        resaults = runCSMToFMS()
+                        if resaults != "error":
+                            moveCMSFiles("a", pathToDatFolder, path)
+                        else:
+                            moveCMSFiles("e", pathToDatFolder, pathToFtp)
                     
-                os.remove(pathToZipFile)
-            except Exception, err:
-                print str(err)
-                with open("C:\\Users\\administrator.NPLATL\\Desktop\\Server Side Scripts\\DatWatcher\\Logs\\errorLog.txt", "a") as logFile:
-                    logFile.write("*****\n" + str(time.strftime("%d/%m/%Y")) + "\n" + str(err) + "\n \n")
-                
+                        os.remove(pathToZipFile)
+
+except Exception, err:
+    print str(err)
+    writeToLog(str(err))
+    sendEmail("Unhandled Exception - " + str(err), "Unhandled Exception", errorEmails)                
